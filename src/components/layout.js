8b3efc04ebe712380230a3c0bb083043543665cf
Riley Rangel <rrangel91@gmail.com>
2020-05-14 12:57:16 -0700

    update layout to use static query hook

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,48 +1,49 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
-import Helmet from 'react-helmet'
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import Helmet from 'react-helmet';
 
-import Sidebar from '.././components/sidebar'
-import '../styles/main.scss'
-import '../styles/fonts/font-awesome/css/font-awesome.min.css'
+import Sidebar from '.././components/sidebar';
+import '../styles/main.scss';
+import '../styles/fonts/font-awesome/css/font-awesome.min.css';
 
-const DefaultLayout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            author
-            description
-            social {
-              twitter
-              facebook
-              linkedin
-              github
-              email
-            }
+/**
+ * Component responsible for rendering the two-column layout - used as the default.
+ *
+ * @type {React.FC}
+ */
+const DefaultLayout = ({ children }) => {
+  const { site } = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          author
+          description
+          social {
+            twitter
+            facebook
+            linkedin
+            github
+            email
           }
         }
       }
-    `}
-    render={data => (
-      <div className="wrapper">
-        <Helmet>
-          <link
-            href="https://fonts.googleapis.com/css?family=Lato|PT+Serif&display=swap"
-            rel="stylesheet"
-          />
-        </Helmet>
-        <Sidebar siteMetadata={data.site.siteMetadata} />
-        <div className="content-box clearfix">{children}</div>
-      </div>
-    )}
-  />
-)
+    }
+  `);
 
-DefaultLayout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  return (
+    <div className="wrapper">
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css?family=Lato|PT+Serif&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
 
-export default DefaultLayout
+      <Sidebar siteMetadata={site.siteMetadata} />
+
+      <div className="content-box clearfix">{children}</div>
+    </div>
+  );
+};
+
+export default DefaultLayout;
